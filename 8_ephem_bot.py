c0ecbf2ca1f0db3bb6b3a24b3d72d961ab030422
El1seius <eliseevsi@gmail.com>
2023-05-22 00:26:07 +0300

    The bot is done

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -15,42 +15,91 @@
 import logging
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import settings
+import ephem
+import datetime
 
-logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO,
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO, 
                     filename='bot.log')
 
-
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
-    }
-}
-
-
 def greet_user(update, context):
-    text = 'Вызван /start'
+    text = 'Здравствуй!'
     print(text)
     update.message.reply_text(text)
 
-
-def talk_to_me(update, context):
-    user_text = update.message.text
-    print(user_text)
+def question_user(update, context):
+    text = 'Введите название планеты на английском:'
+    print(text)
     update.message.reply_text(text)
+
+def talk_to_planet(update, context):
+    user_text = update.message.text.split()
+    print(user_text)
+
+    data_only = datetime.date.today()
+    today_date = f'{data_only.year}/{data_only.month}/{data_only.day}'
+
+    if 'Mercury' in user_text:
+        user_planet = ephem.Mercury(today_date)
+        const_user_planet = ephem.constellation(user_planet)
+        print(const_user_planet)
+        update.message.reply_text(const_user_planet)
+    elif 'Venus' in user_text:
+        user_planet = ephem.Venus(today_date)
+        const_user_planet = ephem.constellation(user_planet)
+        print(const_user_planet)
+        update.message.reply_text(const_user_planet)
+    elif 'Earth' in user_text:
+        user_planet = 'Наша планета не отображается для нас на небе, поэтому не может находиться в каком-либо созвездии))'
+        print(user_planet)
+        update.message.reply_text(user_planet)
+#        user_planet = ephem.Earth(today_date)
+#        const_user_planet = ephem.constellation(user_planet)      
+#        print(const_user_planet)
+#        update.message.reply_text(const_user_planet)
+    elif 'Mars' in user_text:
+        user_planet = ephem.Mars(today_date)
+        const_user_planet = ephem.constellation(user_planet)     
+        print(const_user_planet)
+        update.message.reply_text(const_user_planet)
+    elif 'Jupiter' in user_text:
+        user_planet = ephem.Jupiter(today_date)
+        const_user_planet = ephem.constellation(user_planet)     
+        print(const_user_planet)
+        update.message.reply_text(const_user_planet)
+    elif 'Saturn' in user_text:
+        user_planet = ephem.Saturn(today_date)
+        const_user_planet = ephem.constellation(user_planet)     
+        print(const_user_planet)
+        update.message.reply_text(const_user_planet)
+    elif 'Uranus' in user_text:
+        user_planet = ephem.Uranus(today_date)
+        const_user_planet = ephem.constellation(user_planet)     
+        print(const_user_planet)
+        update.message.reply_text(const_user_planet)
+    elif 'Neptune' in user_text:
+        user_planet = ephem.Neptune(today_date)
+        const_user_planet = ephem.constellation(user_planet)     
+        print(const_user_planet)
+        update.message.reply_text(const_user_planet)
+    else:
+        not_planet = 'Не правильное написанние или это не планета. Напоминание: название планеты должно быть на английском и с заглавной буквы.'
+        print(not_planet)
+        update.message.reply_text(not_planet)
 
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+    mybot = Updater(settings.API_KEY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler("planet", question_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_planet))
+
 
     mybot.start_polling()
     mybot.idle()
+
 
 
 if __name__ == "__main__":
